05513a83b87afdcabaaba0ce819628b0985d19f1
jarekpc <jarekpc@o2.pl>
2022-01-02 17:11:36 +0100

    pierwszy commit

---
+++ src/main/kotlin/com/example/cryptomarket/CryptoMarketApplication.kt
@@ -0,0 +1,20 @@
+package com.example.cryptomarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class CryptoMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoMarketApplication>(*args)
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping
+	fun getHello() = "Hello WOrld!"
+}
